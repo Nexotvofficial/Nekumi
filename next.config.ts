5b3b121d72a07a567411f12747b7b71c4a51666c
Nexotv <121769133+Nexotvofficial@users.noreply.github.com>
2026-09-20 23:18:23 -0500

    security: add X-Robots-Tag to prevent search engine indexing during development

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // Aplica a todas las rutas
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex, nofollow', // Evita que Google indexe la web si está en pruebas
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
